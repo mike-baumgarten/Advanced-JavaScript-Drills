c205fe5e9c30313d9965345d56918960f15616e8
Mike Baumgarten <mike.baumgarten@gmail.com>
2020-08-06 13:41:48 -0600

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('some text');
+let avg = findAvg(2, 2);
+console.log('some text', avg);
+function findAvg(a, b) {
+    console.log('some text');
+    var answer = (a + b) / 2;
+    return answer;
+}
+
+
